93e87d2de01fed15d02416fc6b3d50893eebd11e
aravindVirat <aravind.mallapureddy@juspay.in>
2018-09-26 21:45:19 +0530

    added more testcases:

--- test/Main.purs
+++ test/Main.purs
@@ -7,9 +7,10 @@ import Effect (Effect)
 import Test.Applicative (applicativeTest)
 import Test.CArray (testCArray)
 import Test.Functor (functorTest)
+import Test.Monad (monadTest)
 import Test.Unit.Main (runTest)
 
 main :: Effect Unit
 main = do
-    _ <- sequence $ map runTest [testCArray] 
+    _ <- sequence $ map runTest [testCArray, functorTest,applicativeTest, monadTest] 
     pure unit
